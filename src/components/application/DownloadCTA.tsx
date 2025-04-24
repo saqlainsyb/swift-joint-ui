f2a2a8fd07df3f92e9dcf1e6cc6c050deb5aa0a1
saqlainsyb <saqlainsyb@gmail.com>
2025-04-24 05:41:19 +0530

    Update download cta

--- src/components/application/DownloadCTA.tsx
+++ src/components/application/DownloadCTA.tsx
@@ -3,9 +3,9 @@ import { DownloadCloud } from "lucide-react";
 
 export default function DownloadCTA() {
   return (
-    <section className="bg-[#2F2F2F] py-12 flex justify-center">
-      <Button className="text-xl py-6 px-12 rounded-full bg-gradient-to-r from-blue-500 to-blue-700 hover:opacity-90 transition-opacity">
-        <DownloadCloud className="mr-3" size={24} />
+    <section className="pb-10 flex justify-center">
+      <Button size={'lg'} className="text-xl py-6 px-12 rounded-full bg-gradient-to-r from-blue-500 to-blue-700 hover:opacity-90 transition-opacity">
+        <DownloadCloud className="mr-2" size={24} />
         DOWNLOAD
       </Button>
     </section>
