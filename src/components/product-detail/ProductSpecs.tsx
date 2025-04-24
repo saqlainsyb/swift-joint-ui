17e024c4ec4ba0abaf917118695d9b92b342a758
saqlainsyb <saqlainsyb@gmail.com>
2025-04-24 19:16:28 +0530

    Few Changes in Product Specs

--- src/components/product-detail/ProductSpecs.tsx
+++ src/components/product-detail/ProductSpecs.tsx
@@ -6,8 +6,21 @@ interface ProductSpecsProps {
 
 export default function ProductSpecs({ imageSrc }: ProductSpecsProps) {
   return (
-    <section className="bg-[#383838] px-8 lg:px-30 py-16 flex justify-center">
-      <Image src={imageSrc} alt="Product Specifications" width={800} height={600}/>
+    <section className="bg-[#383838] px-8 lg:px-30 py-16 flex justify-center flex-col items-center">
+      <div className="border-t lg:max-w-xl lg:mr-auto lg:ml-50 mb-10">
+        <h3 className="mt-10">SWIFT JOINT</h3>
+        <p>
+          &quot;Where precision engineering and advanced technology converge,
+          delivering unparalleled efficiency and reliability in electrofusion
+          solutions for the toughest environments.&quot;
+        </p>
+      </div>
+      <Image
+        src={imageSrc}
+        alt="Product Specifications"
+        width={800}
+        height={600}
+      />
     </section>
   );
 }
