d3f2a64a0764cb6e697edbfcdc81f4065e81333a
saqlainsyb <saqlainsyb@gmail.com>
2025-04-29 03:31:06 +0530

    Update product comparison

--- src/components/product-detail/ProductComparison.tsx
+++ src/components/product-detail/ProductComparison.tsx
@@ -6,7 +6,8 @@ interface ProductComparisonProps {
 
 export default function ProductComparison({ imageSrc }: ProductComparisonProps) {
   return (
-    <section className="bg-[#2F2F2F] px-8 lg:px-30 py-16 flex justify-center">
+    <section className="bg-[#2F2F2F] px-8 lg:px-30 py-10 flex flex-col justify-center items-center">
+      <h3 className="uppercase text-2xl md:text-3xl pb-10">Product Comparison</h3>
       <Image src={imageSrc} alt="Product Comparison" width={800} height={600}/>
     </section>
   );
