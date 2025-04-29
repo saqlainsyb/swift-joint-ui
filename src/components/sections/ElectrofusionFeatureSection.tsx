38f516a720e76cf37a0e155fcc9554fbbfb1f445
saqlainsyb <saqlainsyb@gmail.com>
2025-04-29 15:11:17 +0530

    Update Features section on HomePage

--- src/components/sections/ElectrofusionFeatureSection.tsx
+++ src/components/sections/ElectrofusionFeatureSection.tsx
@@ -2,23 +2,22 @@ import Image from "next/image";
 
 export default function ElectrofusionFeatureSection() {
   return (
-    <section className="px-8 lg:px-30 py-16 space-y-20 bg-white text-accent">
+    <section className="px-8 lg:px-30 py-16 space-y-20 text-white">
       <div className="flex flex-col lg:flex-row items-center lg:justify-center gap-12 lg:gap-20 lg:space-x-10">
-        <div className="relative">
-          <div className="bg-gray-100 absolute inset-0 rounded-xl -translate-y-5 translate-x-5" />
+        <div className="relative shadow-[0_60px_100px_-30px_rgba(255,183,76,0.4)] rounded-xl">
           <Image
             src="/images/products/electrofusion-machine.png"
             alt="Electrofusion Machine"
             width={600}
             height={400}
-            className="rounded-xl shadow-lg relative z-10 md:max-w-sm"
+            className="rounded-xl relative z-10 md:max-w-sm object-fill"
           />
         </div>
         <div className="md:max-w-xl">
-          <h2 className="text-3xl lg:text-4xl font-bold text-gray-900 mb-4">
+          <h2 className="text-3xl lg:text-4xl font-bold mb-4">
             We Provide You The Best Electrofusion Machine
           </h2>
-          <p className="text-gray-600 text-lg">
+          <p className="text-lg opacity-85">
             Swift Joint provides the best electrofusion machines, ensuring
             strong, seamless, and reliable pipe connections. Designed for
             efficiency and durability, our machines enhance productivity across
@@ -36,7 +35,7 @@ export default function ElectrofusionFeatureSection() {
           <h2 className="text-3xl font-bold mb-4">
             Reliable Electrofusion In Any Climate
           </h2>
-          <p className="text-gray-600 text-lg">
+          <p className="text-lg opacity-85">
             Swift Joint provides the best electrofusion machines, built for
             reliability in any climate and harsh environments. Designed for
             strength, precision, and efficiency, our machines ensure seamless
@@ -46,13 +45,12 @@ export default function ElectrofusionFeatureSection() {
           </p>
         </div>
         <div className="relative flex gap-2 flex-col md:flex-row w-full md:w-auto justify-stretch">
-          {/* <div className="bg-gray-100 absolute inset-0 rounded-xl -translate-y-5 translate-x-5" /> */}
             <Image
                 src="/images/products/electrofusion-climate-3.png"
                 alt="Electrofusion Machine"
                 width={250}
                 height={350}
-                className="rounded-xl shadow-lg relative z-10 h-full w-full md:w-auto"
+                className="rounded-xl relative z-10 h-full w-full md:w-auto shadow-[0_60px_100px_-65px_rgba(255,183,76,0.4)]"
             />
             <div className="flex flex-col gap-2">
             <Image
@@ -60,14 +58,14 @@ export default function ElectrofusionFeatureSection() {
                 alt="Electrofusion Machine"
                 width={250}
                 height={350}
-                className="rounded-xl shadow-lg relative z-10 h-full w-full md:w-auto"
+                className="rounded-xl shadow-[0_60px_100px_-65px_rgba(255,183,76,0.4)] relative z-10 h-full w-full md:w-auto"
           />
             <Image
                 src="/images/products/electrofusion-climate-2.png"
                 alt="Electrofusion Machine"
                 width={250}
                 height={350}
-                className="rounded-xl shadow-lg relative z-10 w-full md:w-auto"
+                className="rounded-xl shadow-[0_60px_100px_-65px_rgba(255,183,76,0.4)] relative z-10 w-full md:w-auto"
           />
             </div>
         </div>
